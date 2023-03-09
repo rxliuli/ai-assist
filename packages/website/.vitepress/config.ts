b12a2228591b2553f94fd2ceea40f2758e4a88f6
rxliuli <rxliuli@gmail.com>
2023-03-10 01:28:40 +0800

    docs: add edit link

--- packages/website/.vitepress/config.ts
+++ packages/website/.vitepress/config.ts
@@ -12,5 +12,10 @@ export default defineConfig({
     sidebar: [{ text: 'Chat', link: '/chat/' }],
 
     socialLinks: [{ icon: 'github', link: 'https://github.com/rxliuli/ai-assist' }],
+
+    editLink: {
+      pattern: 'https://github.com/rxliuli/ai-assist/edit/master/packages/website/:path',
+      text: 'Edit this page on GitHub',
+    },
   },
 })
