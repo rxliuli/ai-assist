0aa5179f2478aff061bf893bb551d0f5210bc343
rxliuli <rxliuli@gmail.com>
2023-03-16 13:22:23 +0800

    feat: 添加设置页面(wip)

--- packages/chat/src/constants/router.ts
+++ packages/chat/src/constants/router.ts
@@ -1,14 +1,31 @@
 import { createHashHistory, RouteConfig } from '@liuli-util/react-router'
 import { ChatHomeView } from '../views/chat/ChatView'
-import { SettingView } from '../views/setting/SettingView'
+import {
+  SettingHomeView,
+  SettingOpenAPIKeyView,
+  SettingPromptEditView,
+  SettingPromptView,
+  SettingLayoutView,
+} from '../views/setting/SettingView'
 import { SpeakView } from '../views/speak/SpeakView'
 import { AzureSpeechToText } from '../views/test/AzureSpeechToText'
 import { AzureTextToSpeech } from '../views/test/AzureTextToSpeech'
 import { CompleteInputDemo } from '../views/test/CompleteInputDemo'
 
+export const settingRoutes: (RouteConfig & { title: string })[] = [
+  { path: '/setting', component: SettingHomeView, title: 'Setting' },
+  { path: '/setting/open-api-key', component: SettingOpenAPIKeyView, title: 'Open API Key' },
+  { path: '/setting/prompt', component: SettingPromptView, title: 'Prompt' },
+  { path: '/setting/prompt/:promptId', component: SettingPromptEditView, title: 'Prompt Edit' },
+]
+
 export const routes: RouteConfig[] = [
   { path: '/speak', component: SpeakView },
-  { path: '/settings', component: SettingView },
+  {
+    path: '/setting',
+    component: SettingLayoutView,
+    children: settingRoutes,
+  },
   { path: '/:sessionId', component: ChatHomeView },
   { path: '/', component: ChatHomeView },
 ]
--- packages/chat/src/views/setting/SettingView.tsx
+++ packages/chat/src/views/setting/SettingView.tsx
@@ -1,5 +1,73 @@
-import { observer } from 'mobx-react-lite'
+import classNames from 'classnames'
+import { observer, useLocalStore } from 'mobx-react-lite'
+import css from './SettingView.module.css'
+import { Link, RouterView, useLocation } from '@liuli-util/react-router'
+import { router, settingRoutes } from '../../constants/router'
+import { FormEvent, useMemo } from 'react'
 
-export const SettingView = observer(() => {
-  return <div>settings</div>
+export const SettingOpenAPIKeyView = observer(() => {
+  const apiKey = useLocalStore(() => ({ value: '' }))
+  function onSave(ev: FormEvent) {
+    ev.preventDefault()
+    localStorage.setItem('OpenAPIKey', apiKey.value)
+    alert('Save success')
+    apiKey.value = ''
+  }
+  return (
+    <form>
+      <div>
+        <label htmlFor={'OpenAPIKey'}>OpenAPIKey:</label>
+        <input
+          type={'password'}
+          id={'OpenAPIKey'}
+          value={apiKey.value}
+          onChange={(ev) => (apiKey.value = ev.target.value)}
+        ></input>
+      </div>
+      <div>
+        <button onClick={onSave}>Save</button>
+      </div>
+    </form>
+  )
+})
+
+export const SettingPromptView = observer(() => {
+  return <div>SettingPromptView</div>
+})
+
+export const SettingPromptEditView = observer(() => {
+  return (
+    <form>
+      <div>
+        <button>SettingPromptEditView</button>
+      </div>
+    </form>
+  )
+})
+
+export const SettingHomeView = observer(() => {
+  return (
+    <div>
+      <Link to={'/setting/open-api-key'}>OpenAPIKey</Link>
+      <Link to={'/setting/prompt'}>Prompt</Link>
+    </div>
+  )
+})
+
+export const SettingLayoutView = observer(() => {
+  const title = useMemo(
+    () => settingRoutes.find((it) => it.path === router.location.pathname)?.title ?? 'Setting',
+    [useLocation()],
+  )
+  return (
+    <div className={classNames('container', css.settings)}>
+      <header className={css.header}>
+        <button onClick={() => router.back()}>Back</button>
+        <h2>{title}</h2>
+      </header>
+      <main>
+        <RouterView></RouterView>
+      </main>
+    </div>
+  )
 })
