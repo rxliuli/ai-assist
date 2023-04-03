bb58496d1b8a04ec9b57674ecabe9eaba545d1ea
rxliuli <rxliuli@gmail.com>
2023-04-04 01:17:14 +0800

    perf: 使用缓存提升 token 校验相关的性能

--- packages/chat-server/src/services/user/auth.ts
+++ packages/chat-server/src/services/user/auth.ts
@@ -3,6 +3,8 @@ import { TokenModel, User } from '../../constants/db'
 import bcrypt from 'bcrypt'
 import { v4 } from 'uuid'
 import dayjs from 'dayjs'
+import LRUCache from 'lru-cache'
+import { logger } from '../../constants/logger'
 
 export const SaltRounds = 10
 
@@ -17,34 +19,62 @@ export async function generateToken(user: User): Promise<string> {
   return token
 }
 
+const tokenCache = new LRUCache<string, { userId: string; expirationTime: Date }>({
+  max: 1000, // 缓存最大条目数，根据实际需求调整
+  ttl: 1000 * 60 * 60, // 缓存有效期，单位：毫秒，这里设置为 1 小时
+  updateAgeOnGet: false,
+  updateAgeOnHas: false,
+})
+
+const tokenUpdateThreshold = 7 // 设置 token 更新阈值，单位：天
+const tokenExpiratDays = 30
+
 export async function validateToken(token: string): Promise<boolean> {
-  const r = await TokenModel.findOne({
-    where: { accessToken: token },
-  })
+  let r = tokenCache.get(token)
   if (!r) {
+    const tokenModel = await TokenModel.findOne({
+      where: { accessToken: token },
+    })
+    if (!tokenModel) {
+      return false
+    }
+    r = tokenModel.get()
+    tokenCache.set(token, r)
+  }
+  if (dayjs(r.expirationTime).isBefore(dayjs())) {
     return false
   }
-  if (dayjs(r.get().expirationTime).isBefore(dayjs())) {
-    return false
+  // 检查 token 是否接近过期，如果是，则更新数据库中的过期时间
+  const daysToExpiration = dayjs(r.expirationTime).diff(dayjs(), 'day')
+  if (daysToExpiration < tokenUpdateThreshold) {
+    await TokenModel.update(
+      { expirationTime: dayjs().add(tokenExpiratDays, 'day').toDate() },
+      { where: { accessToken: token } },
+    )
   }
-  await r.set('expirationTime', dayjs().add(30, 'day').toDate()).save()
   return true
 }
 
 export async function revokeToken(token: string): Promise<void> {
+  tokenCache.delete(token)
   await TokenModel.destroy({
     where: { accessToken: token },
   })
 }
 
 export async function getUserIdByToken(token: string): Promise<string | undefined> {
-  const r = await TokenModel.findOne({
-    where: { accessToken: token },
-  })
+  let r = tokenCache.get(token)
   if (!r) {
-    return
+    const tokenModel = await TokenModel.findOne({
+      where: { accessToken: token },
+    })
+    if (!tokenModel) {
+      return
+    }
+    r = tokenModel.get()
+    tokenCache.set(token, r)
   }
-  return r.get().userId
+  return r.userId
 }
 
 export function auth(): Middleware {
